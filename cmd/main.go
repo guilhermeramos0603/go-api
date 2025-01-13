b2fe43264f9de00836ca1a5e7f5b54de13280fed
guilhermeramos0603 <guilherme.silvano@hotmail.com>
2025-01-13 19:50:02 -0300

    backup

--- cmd/main.go
+++ cmd/main.go
@@ -5,20 +5,25 @@ import (
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/guilhermeramos0603/go-api.git/cmd/api"
+	"github.com/guilhermeramos0603/go-api.git/config"
 	"github.com/guilhermeramos0603/go-api.git/db"
 )
 
 func main() {
 	db, err := db.NewMySQLStorage(mysql.Config{
-		User:                 "root",
-		Passwd:               "asd",
-		Addr:                 "",
-		DBName:               "",
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
 		Net:                  "tcp",
 		AllowNativePasswords: true,
 		ParseTime:            true,
 	})
-	server := api.NewAPIServer(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
